18dbe6d2741456d51d52b150dfc89ca8aea204f2
Renu Meshram <renumeshram24@navgurukul.org>
2025-09-12 11:20:04 +0530

    Created schema for zod validation

--- client/src/schemas/authSchemas.js
+++ client/src/schemas/authSchemas.js
@@ -7,9 +7,38 @@ export const loginSchema = z.object({
 
 export const userRegisterSchema = z.object({
   name: z.string().min(1, "Name is required"),
-  email: z.string().email("Invalid email address"),
-  sapId: z.string().min(1, "SAP ID is required"),
-  departmentId: z.string().min(1, "Department is required"),
-  sectionId: z.string().min(1, "Section is required"),
-  password: z.string().min(6, "Password must be at least 6 characters")
+  email: z
+  .string()
+  .email("Invalid email address")
+  .regex(
+    /^[a-zA-Z0-9._%+-]+@nmdc\.co\.in$/,
+    "Email ID must be a valid NMDC mail Id."),
+  
+  // SAP ID: 8-digit number between 10000000 and 99999999
+  sapId: z
+    .number({
+      required_error: "SAP ID is required",
+      invalid_type_error: "SAP ID must be a number",
+    })
+    .min(10000000, "SAP ID must be 8 digits")
+    .max(99999999, "SAP ID must be 8 digits"),
+
+  location: z.string().nonempty("Location is required"),
+  department: z.string().min(1, "Department is required"),
+  section: z.string().min(1, "Section is required"),
+
+  // Password: at least 1 uppercase, 1 lowercase, 1 digit, 1 special char, min length 8
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain uppercase, lowercase, number, and special character"
+    ),
+
+  confirmPassword: z.string().min(8, "Confirm Password must be at least 8 characters")
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords must match",
+  path: ["confirmPassword"],
 });
--- client/src/schemas/requestSchemas.js
+++ client/src/schemas/requestSchemas.js
@@ -1,8 +1,15 @@
 import { z } from 'zod';
 
 export const requestSchema = z.object({
-  categoryId: z.string().min(1, "Category is required"),
-  assetId: z.string().min(1, "Asset is required"),
+ location: z.string().nonempty("Location is required"),
+  department: z.string().nonempty("Department is required"),
+  section: z.string().nonempty("Section is required"),
+  category: z.string().nonempty("Category is required"),
+  asset: z.string().nonempty("Asset is required"),
   quantity: z.number().min(1, "Quantity must be at least 1"),
-  comments: z.string().optional()
+  requestDate: z.string().nonempty("Request date is required"),
+  comments: z.string().optional(),
+  // requestDate: z.date().refine(date => date <= new Date(), {
+  //   message: "Request date cannot be in the future" 
+  // }),
 });
